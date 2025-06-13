2a0991a182b5234b5ef8a40602ed918edafd18fd
Rian Dantas Landim <rianlandim222@gmail.com>
2025-06-13 13:48:32 -0300

    feat: add the new backend url

--- utils/api.ts
+++ utils/api.ts
@@ -3,6 +3,7 @@ import { getBaseUrl } from "./base-url";
 
 const api = axios.create({
   baseURL: getBaseUrl(), // Altere para a URL do seu backend se necessário
+  // baseURL: "https://dont-sleepy-back.fly.dev",
 });
 
 export default api;
